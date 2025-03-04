35f97f5a7f766d5aff48076c2b6ab328582ce1e9
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-03-04 11:42:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+	let boldWords = document.querySelectorAll("strong");
+	boldWords.forEach(word =>{
+		word.style.color = "rgb(0, 128, 0)";
+	});
+	// for (let i = 0; i < boldWords.length; i++) {
+ //    boldWords[i].style.color = "rgb(0, 0, 0)";
+// }
 
 }
 
-
 function return_normal() {
     //Write your code here
+	let boldWords = document.querySelectorAll("strong");
+	boldWords.forEach(word =>{
+		word.style.color = "rgb(0, 0, 0)";
+	});
 
     
 }
